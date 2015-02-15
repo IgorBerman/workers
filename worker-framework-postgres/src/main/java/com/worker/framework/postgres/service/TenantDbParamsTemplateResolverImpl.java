e6367dc3ddecf98a574bc23c61f1665e7160e057
rapen <igor.berman@gmail.com>
2015-02-15 23:45:06 +0200

    fixing configuration

---
+++ worker-framework-postgres/src/main/java/com/worker/framework/postgres/service/TenantDbParamsTemplateResolverImpl.java
@@ -0,0 +1,34 @@
+package com.worker.framework.postgres.service;
+
+import com.tenant.framework.TenantDbParamsTemplateResolver;
+
+
+public class TenantDbParamsTemplateResolverImpl implements TenantDbParamsTemplateResolver {
+
+    private static final String TENANT_ID_PARAM = "$[tenantId]";
+
+    @Override
+    public String resolveUrl(String urlTemplate, String tenantId) {
+        return replaceDefault(urlTemplate, tenantId);
+    }
+
+    @Override
+    public String resolveUser(String userTemplate, String tenantId) {
+        return replaceDefault(userTemplate, tenantId);
+    }
+
+    @Override
+    public String resolvePassword(String passwordTemplate, String tenantId) {
+        return replaceDefault(passwordTemplate, tenantId);
+    }
+
+    @Override
+    public String resolveDbName(String dbNameTemplate, String tenantId) {
+        return replaceDefault(dbNameTemplate, tenantId);
+    }
+
+    private String replaceDefault(String template, String tenantId) {
+        return template.replace(TENANT_ID_PARAM, tenantId);
+    }
+
+}
